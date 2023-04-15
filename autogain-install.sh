2455aeb557601d2797a48910fca7ec52461e385e
pavelt <pavel@onoapps.com>
2023-04-16 00:40:44 +0300

    optimized values

--- autogain-install.sh
+++ autogain-install.sh
@@ -42,8 +42,8 @@ fi
 mkdir -p /usr/local/bin
 cat >/usr/local/bin/autogain1090 <<"EOF"
 #!/bin/bash
-low=0.5
-high=7.0
+low=1.5
+high=10.0
 ga=(0.0 0.9 1.4 2.7 3.7 7.7 8.7 12.5 14.4 15.7 16.6 19.7 20.7 22.9 25.4 28.0 29.7 32.8 33.8 36.4 37.2 38.6 40.2 42.1 43.4 43.9 44.5 48.0 49.6 -10)
 source /etc/default/autogain1090
 tmp=/var/tmp/autogain1090
@@ -195,8 +195,8 @@ if ! [ -f $config_file ]; then
 	cat >/etc/default/autogain1090 <<"EOF"
 #!/bin/bash
 
-low=0.5
-high=7.0
+low=1.5
+high=10.0
 
 ga=(0.0 0.9 1.4 2.7 3.7 7.7 8.7 12.5 14.4 15.7 16.6 19.7 20.7 22.9 25.4 28.0 29.7 32.8 33.8 36.4 37.2 38.6 40.2 42.1 43.4 43.9 44.5 48.0 49.6 -10)
 EOF
